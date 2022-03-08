f447b436a19ca3748eeba52175112d14e4b372c1
Riley Line <line.riley@gmail.com>
2022-03-07 21:38:29 -0500

    Added social media menu in customizer

--- themes/rol/inc/customizer.php
+++ themes/rol/inc/customizer.php
@@ -30,6 +30,73 @@ function rol_customize_register( $wp_customize ) {
 			)
 		);
 	}
+
+	$wp_customize->add_panel( 'rol_social_media', array(
+		'title' => esc_html__( 'Social Media', 'rol') 
+	) );
+
+	// Etsy
+	$wp_customize->add_section( 'rol_etsy', array(
+		'title' => esc_html__( 'Etsy', 'rol'),
+		'panel' => 'rol_social_media'
+	) );
+
+	$wp_customize->add_setting( 'rol_etsy_title');
+	$wp_customize->add_control( 'rol_etsy_title', array(
+		'label' => 'Title',
+		'description' => "Enter your link title",
+		'section' => 'rol_etsy'
+	) );
+
+	$wp_customize->add_setting( 'rol_etsy_url');
+	$wp_customize->add_control( 'rol_etsy_url', array(
+		'label' => 'URL',
+		'description' => "Enter your Etsy url",
+		'section' => 'rol_etsy'
+	) );
+
+
+	// Insta
+	$wp_customize->add_section( 'rol_insta', array(
+		'title' => esc_html__( 'Instagram', 'rol'),
+		'panel' => 'rol_social_media'
+	) );
+
+	$wp_customize->add_setting( 'rol_insta_title');
+
+	$wp_customize->add_control( 'rol_insta_title', array(
+		'label' => 'Title',
+		'description' => "Enter your link title",
+		'section' => 'rol_insta'
+	) );
+	$wp_customize->add_setting( 'rol_insta_url');
+
+	$wp_customize->add_control( 'rol_insta_url', array(
+		'label' => 'URL',
+		'description' => "Enter your Instagram url",
+		'section' => 'rol_insta'
+	) );
+
+	// Facebook
+	$wp_customize->add_section( 'rol_facebook', array(
+		'title' => esc_html__( 'Facebook', 'rol'),
+		'panel' => 'rol_social_media'
+	) );
+
+	$wp_customize->add_setting( 'rol_facebook_title');
+
+	$wp_customize->add_control( 'rol_facebook_title', array(
+		'label' => 'Title',
+		'description' => "Enter your link title",
+		'section' => 'rol_facebook'
+	) );
+	$wp_customize->add_setting( 'rol_facebook_url');
+
+	$wp_customize->add_control( 'rol_facebook_url', array(
+		'label' => 'URL',
+		'description' => "Enter your Facebook url",
+		'section' => 'rol_facebook'
+	) );
 }
 add_action( 'customize_register', 'rol_customize_register' );
 
