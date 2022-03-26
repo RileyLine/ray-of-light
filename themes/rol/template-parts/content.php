0aaf0782787092ac48f5b5eb5e699f520e2958f4
Riley Line <line.riley@gmail.com>
2022-03-26 15:52:36 -0400

    Removed categorization footer

--- themes/rol/template-parts/content.php
+++ themes/rol/template-parts/content.php
@@ -57,7 +57,4 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php rol_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
